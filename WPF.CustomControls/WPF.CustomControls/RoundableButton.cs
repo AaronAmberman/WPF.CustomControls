308beda552f7be362ca01af2cd864b78724278f7
Aaron Amberman <aaronamberman@gmail.com>
2022-04-01 14:57:27 -0400

    Added RoundableButton

---
+++ WPF.CustomControls/WPF.CustomControls/RoundableButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.CustomControls
+{
+    /// <summary>A normal button with a CornerRadius Property.</summary>
+    public class RoundableButton : Button
+    {
+        #region Properties
+
+        /// <summary>Gets or sets the corner radius for the button.</summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(RoundableButton), new PropertyMetadata(null));
+
+        #endregion
+
+        #region Constructors
+
+        static RoundableButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundableButton), new FrameworkPropertyMetadata(typeof(RoundableButton)));
+        }
+
+        #endregion
+    }
+}
