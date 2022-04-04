c6eca16a818350c2b10f19d5b5642d602a45fbe9
Aaron Amberman <aaronamberman@gmail.com>
2022-04-04 18:38:10 -0400

    Added PopupExpander

---
+++ WPF.CustomControls/WPF.CustomControls/PopupExpander.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.CustomControls
+{
+    /// <summary>An Expander that presents its content in a Popup.</summary>
+    public class PopupExpander : Expander
+    {
+        #region Constructors
+
+        static PopupExpander()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PopupExpander), new FrameworkPropertyMetadata(typeof(PopupExpander)));
+        }
+
+        #endregion
+    }
+}
