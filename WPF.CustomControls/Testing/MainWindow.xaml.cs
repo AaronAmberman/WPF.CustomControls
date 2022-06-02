756eba8d5fb96910334947da519a3deb741f930a
Aaron Amberman <aaronamberman@gmail.com>
2022-06-02 18:53:40 -0400

    Updates to testing window

--- WPF.CustomControls/Testing/MainWindow.xaml.cs
+++ WPF.CustomControls/Testing/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
 
 namespace Testing
 {
@@ -7,6 +10,33 @@ namespace Testing
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] setDirs = Directory.GetDirectories(@"C:\Users\Aaron\AppData\Local\MTGA-DeckMaster\CardImages");
+
+                foreach (string dir in setDirs)
+                {
+                    string[] cardDirs = Directory.GetDirectories(dir);
+
+                    foreach (string cardDir in cardDirs)
+                    {
+                        string[] files = Directory.GetFiles(cardDir);
+
+                        foreach (string file in files)
+                        {
+                            ZoomableListView.Items.Add(file);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
         }
     }
 }
