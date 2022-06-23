22bd1cb045f8146e2897c9b51a401155a7ffc197
Aaron Amberman <aaronamberman@gmail.com>
2022-06-23 11:39:07 -0400

    Removed comment

--- WPF.CustomControls/WPF.CustomControls/ZoomableListBox.cs
+++ WPF.CustomControls/WPF.CustomControls/ZoomableListBox.cs
@@ -138,7 +138,6 @@ namespace WPF.CustomControls
 
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
